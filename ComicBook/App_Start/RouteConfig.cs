d617c7edbd34b6169aebb3d30a4eaad91c28904c
John Allen <cmykfan@earthlink.net>
2018-07-19 21:50:33 -0400

    First wave of code, started creating Views and foundation.

---
+++ ComicBook/App_Start/RouteConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ComicBook//.App_Start  May not need the .App_Start.
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute("Default", "{controller}/{action}/{id}", new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+        }
+    }
+}
---
+++ ComicBook/Models/TitleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBook.Models
+{
+    public class TitleViewModel
+    {
+        public int? TitleId { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string FullName => "Title" + Title + " " + "Artist" + Artist;
+    }
+}
